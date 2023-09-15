b761f727d87c5fd72bc22da98a5e074e305f2ad0
Jean Viscki <jeanviscki@yahoo.com.br>
2023-09-15 05:02:50 -0300

    test post

---
+++ VSCODING coisas feitas/Estudando HTML,CSS,JAVA/Matematica aplicada .py/Aula 1 teoria .py
@@ -0,0 +1,16 @@
+idadeAna=31
+idadeBeatriz=29
+idadeAna>idadeBeatriz
+
+
+expression_result = (3 + 2) == 8
+print(expression_result)
+
+
+
+M=float(input('Média obtida: ')) 
+F=float(input('Total de faltas: '))
+if M>=70 and F<=20:
+  print('Aprovado')
+else:
+  print('Reprovado')
---
+++ VSCODING coisas feitas/Estudando HTML,CSS,JAVA/Matematica aplicada .py/TEste.py
@@ -0,0 +1,24 @@
+from string import ascii_uppercase
+
+a=list(ascii_uppercase)
+
+m=input('Digite a mensagem: ')
+
+m=m.upper()
+
+mc=""
+
+for l in m:
+
+  i=ord(l)-65
+
+  if l in a:
+
+    mc+=a[(i+5)%26]
+
+  else:
+
+    l
+
+print(f'Mensagem criptografada: {mc}')
+
